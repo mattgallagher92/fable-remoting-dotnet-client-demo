a1ca32f5c51cd5b4396776ae9926a8c5e9e64da9
Matt Gallagher <matt@compositional-it.com>
2021-07-30 11:11:51 +0100

    Implement DotnetClient demo

--- src/DotnetClient/Program.fs
+++ src/DotnetClient/Program.fs
@@ -1,13 +1,28 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open Fable.Remoting.DotnetClient
+open Shared
 
-open System
+let todosApi =
+    Remoting.createApi "http://localhost:8085"
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.buildProxy<ITodosApi>
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let printTodos () = async {
+    let! todos = todosApi.getTodos ()
+
+    let mutable itemNo = 1
+    for todo in todos do
+        printfn "Todo %i: %s" itemNo todo.Description
+        itemNo <- itemNo + 1
+}
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    let mutable shouldContinue = true
+    while shouldContinue do
+        printTodos () |> Async.RunSynchronously
+        
+        printfn "%sPress 'q' to exit or any other key to fetch and print the latest todos.%s"
+            System.Environment.NewLine System.Environment.NewLine
+        let k = System.Console.ReadKey()
+        shouldContinue <- k.KeyChar <> 'q'
+    0
